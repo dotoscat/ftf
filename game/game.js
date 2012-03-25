e5e6d0c2c5bbb027e3532ca8bd60c147bcd583cf
Oscar (.teri) Triano <teritriano@gmail.com>
2012-03-26 01:27:01 +0200

    Events and main scene almost completed

--- game/game.js
+++ game/game.js
@@ -1,7 +1,14 @@
 var screen;
 var images;
+var meters = 250;
+var hours = 0;
+var min = 0;
+var secs = 0;
+var msecs = 0;
 
 var currentscene;
+
+var key_enter = 13;
 
 function load(){
     screen = document.getElementById("screen").getContext('2d');
@@ -9,21 +16,32 @@ function load(){
     setInterval(game_run, 1000/60);
 }
 
+function keydown(key){
+    if (currentscene == main && key.keyCode == key_enter){
+        main.title = "Game started!";
+    }
+}
+
 function draw_text(text, color, x, y){
     screen.fillStyle = color;
     screen.fillText(text, x, y);
 }
 
-function Screen(){
+function Main(){
     this.title = "Free For Fall";
     this.Run = function(){
-        draw_text(this.title, "#00FF00", 64, 64);
+        draw_text(this.title, "black", 64, 64);
+        draw_text(meters + " meters/"+hours+":"+min+":"+secs+":"+msecs, "black", 120, 120);
+        draw_text("Press enter to start", "black", 120, 240);
     }
 }
 
-currentscene = new Screen();
+currentscene = new Main();
+var main = currentscene;
+document.onkeydown = keydown;
 
 function game_run(){
-    screen.clearRect(0, 0, 640, 480);
+    screen.fillStyle = "skyblue";
+    screen.fillRect(0, 0, 640, 480);
     currentscene.Run();
 }

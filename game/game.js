9d21207351513e89ad61bd61130fdf27a986b3e4
Oscar (.teri) Triano <teritriano@gmail.com>
2012-03-26 12:07:14 +0200

    var for the scenes. Starting the engine scene and the sprite class

--- game/game.js
+++ game/game.js
@@ -8,12 +8,11 @@ var secs = 0;
 var msecs = 0;
 var km_h = 0;
 
-var currentscene;
-
 var key_enter = 13;
 
 function load(){
     screen = document.getElementById("screen").getContext('2d');
+    screen.save();//save initial state 1
     images = new Array();
     sounds = new Array();
     setInterval(game_run, 1000/60);
@@ -28,6 +27,31 @@ function draw_text(text, color, x, y){
     screen.fillText(text, x, y);
 }
 
+//Sprite class
+function Sprite(image){
+    this.image = image;
+    this.alpha = 1.0;
+    this.x = 0;
+    this.y = 0;
+    this.Draw = function(){
+        screen.globalAlpha = this.alpha;
+        screen.drawImage(this.image, x, y);
+        screen.globalAlpha = 1.0;
+    }
+    this.setAlpha = function(alpha){
+        this.alpha = this.alpha / 255.0;}
+}
+
+//Engine scene
+function Engine(){
+    this.something = 12;
+    this.Run = function(){
+        draw_text("Put some action here!", "black", 120, 120);
+    }
+    this.keydown = function(){
+    }
+}
+
 //Main scene
 function Main(){
     this.title = "Free For Fall";
@@ -39,17 +63,14 @@ function Main(){
     }
     this.keydown = function(key){
         if (key.keyCode == key_enter){
-            this.title = "Game started!";
+            currentscene = engine;
         }
     }
 }
 
-//Engine scene
-function Engine(){
-}
-
-currentscene = new Main();
-var main = currentscene;
+var main = new Main();
+var engine = new Engine();
+var currentscene = main;
 document.onkeydown = keydown;
 
 function game_run(){

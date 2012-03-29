6e7d88ea4979f6b9793b6314f804443dc431d629
Oscar (.teri) Triano <teritriano@gmail.com>
2012-03-29 11:12:09 +0200

    Added collisions

---
+++ code/collisions.cpp
@@ -0,0 +1,18 @@
+#include "collisions.hpp"
+
+namespace fff{
+    
+    namespace collisions{
+        
+        namespace types{
+            cpCollisionType kitty = 1;
+            cpCollisionType explosive = 2;
+        }
+        
+        namespace groups{
+            cpGroup explosive = 1;
+        }
+        
+    }
+    
+}
---
+++ code/collisions.hpp
@@ -0,0 +1,23 @@
+#ifndef _collisions_
+#define _collisions_
+
+#include <chipmunk/chipmunk.h>
+
+namespace fff{
+    
+    namespace collisions{
+        
+        namespace types{
+            extern cpCollisionType kitty;
+            extern cpCollisionType explosive;
+        }
+        
+        namespace groups{
+            extern cpGroup explosive;
+        }
+        
+    }
+    
+}
+
+#endif

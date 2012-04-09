3a9a01e953c981c5f3b1a7e97613123534073c8f
Oscar (.teri) Triano <teritriano@gmail.com>
2012-04-09 22:16:52 +0200

    Buffer increased to 13 (12+\0)

--- code/engine.cpp
+++ code/engine.cpp
@@ -159,7 +159,7 @@ void fff::engine::Run(sf::RenderTarget &rendertarget){
     sf::FloatRect camerarect;
     sf::Vector3f listenerpos;
     sf::Vector2f camerapos;
-    char buffer[12] = {0};
+    char buffer[13] = {0};
     sf::Uint32 currenttime = game.realwindow.GetFrameTime();
     switch(status){
         case running:
@@ -188,10 +188,10 @@ void fff::engine::Run(sf::RenderTarget &rendertarget){
             }
             //---
                         
-            snprintf(buffer, 12, "%g", kitty.getVerticalSpeed() );
+            snprintf(buffer, 13, "%g", kitty.getVerticalSpeed() );
             speed.SetString(buffer);
             
-            snprintf(buffer, 12, "%g", PIXELSTOMETERS(-kitty.getHeight()) );
+            snprintf(buffer, 13, "%g", PIXELSTOMETERS(-kitty.getHeight()) );
             height.SetString(buffer);
             
             time += currenttime;
